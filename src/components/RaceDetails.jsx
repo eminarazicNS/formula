cbca1f2a833864e76b55375d24f24b49823d4d47
emina.razic <emina.razic@gmail.com>
2026-05-19 17:38:15 +0200

    Installed new package

--- src/components/RaceDetails.jsx
+++ src/components/RaceDetails.jsx
@@ -1,6 +1,135 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router";
+import Loader from "./Loader";
 
 export default function RaceDetails() {
+    const [qualifying, setQualifying] = useState(null);
+    const [races, setRaces] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getRaceDetails();
+    }, []);
+
+    const params = useParams();
+    console.log("params", params);
+
+    const getRaceDetails = async () => {
+        const qualifyingUrl = `https://api.jolpi.ca/ergast/f1/2013/${params.id}/qualifying.json`;
+        const racesUrl = `https://api.jolpi.ca/ergast/f1/2013/${params.id}/results.json`;
+
+        const qualifyingResponse = await axios.get(qualifyingUrl);
+        const racesResponse = await axios.get(racesUrl);
+
+        console.log("qualifying Response", qualifyingResponse.data.MRData.RaceTable.Races[0]);
+        console.log("races Response", racesResponse.data.MRData.RaceTable.Races[0]);
+
+        setQualifying(qualifyingResponse.data.MRData.RaceTable.Races[0]);
+        setRaces(qualifyingResponse.data.MRData.RaceTable.Races[0]);
+
+        setLoading(false);
+    }
+
+    const bestTime = (q1, q2, q3) => {
+        let min = q1;
+        if(q2 < min) {
+            min = q2;
+        } 
+        if(q3 < min) {
+            min = q3;
+        }
+        return min;
+    };
+
+    // bestTime(430,200,556);
+
+    if(loading) {
+        return <Loader />
+    }
+
     return (
-        <h2>RaceDetails</h2>
+        <div className="wrapper">
+            <div className="col1">
+                <img src="../img/logo.png" alt="Logo" />
+                <div className="vNav">
+                    <ul>
+                        <li><Link to="/">Drivers</Link></li>
+                        <li><Link to="/teams">Teams</Link></li>
+                        <li><Link to="/races">Races</Link></li>
+                    </ul>
+                </div>
+            </div>
+           
+
+            <div className="col2">
+                {/* <h2>RaceDetails</h2> */}
+                <div className="details">
+                    <img src="../img/Kaciga.png" alt="Country picture" style={{width: 200}}/>
+                    <p>Country: {qualifying.raceName}</p>
+                    <p>Country: {qualifying.Circuit.Location.country} </p>
+                    <p>Location: {qualifying.Circuit.Location.country}</p>
+                    <p>Date: {qualifying.date}</p>
+                    <a href={qualifying.url} target="blanc">Full Report</a>
+                </div>
+
+                <div className="results">
+                    <h2>Qualifying Results</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Pos</th>
+                                <th>Driver</th>
+                                <th>Team</th>
+                                <th>Best time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {qualifying.QualifyingResults.map((qualifier) => {
+                                return (
+                                    <tr key={qualifier.position}>
+                                        <td>{qualifier.position}</td>
+                                        <td>{qualifier.Driver.familyName}</td>
+                                        <td>{qualifier.Constructor.name}</td>
+                                        <td>{bestTime(qualifier.Q1, qualifier.Q2, qualifier.Q3)}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+
+                   <div className="results">
+                    <h2>Race Results</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Pos</th>
+                                <th>Driver</th>
+                                <th>Team</th>
+                                <th>Result</th>
+                                <th>Points</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {races.Results.map((race) => {
+                                return (
+                                    <tr key={race.position}>
+                                        <td>{race.position}</td>
+                                        <td>{race.Driver.familyName}</td>
+                                        <td>{race.Constructor.name}</td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+
+
     );
 }
--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -2,29 +2,53 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router";
 import Loader from "./Loader";
 import axios from "axios";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 
 export default function TeamDetails() {
     const [teamDetails, setTeamDetails] = useState({});
-    const [driverRaces, setDriverRaces] = useState({});
+    const [teamRaces, setTeamRaces] = useState({});
     const [loading, setLoading] = useState(true);
+    const [firstDriver,setFirstDriver] = useState("");
+    const [secondDriver,setSecondDriver] = useState("");
 
     useEffect(() => {
         console.log("useEffect");
-        getResults();
+        getTeams();
     }, []);
 
     const params = useParams();
     console.log("team det params ", params);
 
-    const getResults = async () => {
-        console.log("getResults");
-        const url = `https://api.jolpi.ca/ergast/f1/2013/constructors/${params.id}/constructorStandings.json`;
-        const responseDetails = await axios.get(url);
-        console.log("responseDetails ", responseDetails);
-        // console.log(responseDetails.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
-        // setTeamDetails(responseDetails.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
-        setLoading(false);
+
+    const getTeams = async () => {
+        console.log("getTeams");
+        const teamStandingUrl = `https://api.jolpi.ca/ergast/f1/2013/constructors/${params.id}/constructorStandings.json`;
+
+        const teamRacesUrl = `https://api.jolpi.ca/ergast/f1/2013/constructors/${params.id}/results.json`;
+
+        const teamStandingResponse = await axios.get(teamStandingUrl);
+
+        const teamRacesResponse = await axios.get(teamRacesUrl);
+
+        //console.log("teamStandingResponse ", teamStandingResponse);
+
+        //console.log(teamStandingResponse.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0]);
+
+        //console.log("teamRacesResponse ", teamRacesResponse);
+
+        console.log(teamRacesResponse.data.MRData.RaceTable.Races);
+
+        setTeamDetails(teamStandingResponse.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0]);
+
+        setTeamRaces(teamRacesResponse.data.MRData.RaceTable.Races);
+
+        //table races header
+        setFirstDriver(teamRacesResponse.data.MRData.RaceTable.Races[0].Results[0].Driver.familyName);
+        setSecondDriver(teamRacesResponse.data.MRData.RaceTable.Races[0].Results[1].Driver.familyName);
+        //console.log("firstDriver ",firstDriver, " secondDriver ",secondDriver);
+        
+        setLoading(false); 
     }
 
 
@@ -46,7 +70,71 @@ export default function TeamDetails() {
             </div>
 
             <div className="col2">
-                <h2>DRIVERS DETAILS</h2>
+                <div className="vNav">
+                    <h2>TEAM DETAILS - vertikalna navigacija</h2>
+                </div>
+                <div className="container-details">
+                    <div className="about">
+                        <div className="about-up">
+                            <p>Team: {teamDetails.Constructor.name}</p>
+                            <p>Country: {teamDetails.Constructor.nationality}</p>
+                            <p>Points: {teamDetails.points}</p>
+                            <p>History: <a href={teamDetails.Constructor.teamStandingUrl} target="_blank"><OpenInNewIcon />
+                            </a></p>
+                        </div>
+                        <div className="about-down">
+                            about-down
+                        </div>
+                    </div>
+                    <div className="table-details">
+                        <div>
+                            <h2>Formula 1 2013 Results</h2>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Round</th>
+                                        <th>Grand Prix</th>
+                                        <th>{firstDriver}</th>
+                                        <th>{secondDriver}</th>
+                                        <th>Points</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {teamRaces.map((race)=>{
+                                        return (
+                                              <tr key={race.round}>
+                                                <td>{race.round}</td>
+                                                <td>{race.raceName}</td>
+                                               <td>{race.Results[0].position}</td>
+                                               <td>{race.Results[1].position}</td>
+                                               <td>{Number(race.Results[0].points) + Number(race.Results[1].points)}</td>
+                                            </tr>
+                                        );
+                                    })} 
+
+
+                                    {/* 
+                                    
+                                     {teamRaces.map((race) => {
+                                        return (
+                                            <tr key={race.round}>
+                                                <td>{race.round}</td>
+                                                <td>{race.raceName}</td>
+                                                <td>{race.Results[0].Constructor.name}</td>
+                                                <td>{race.Results[0].grid}</td>
+                                                <td>{race.Results[0].position}</td>
+                                            </tr>
+                                        )
+                                    })} 
+                                    */}
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     );
