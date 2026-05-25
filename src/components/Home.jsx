2d99bc0ec8bdd6a9f64ff824c29d1c960f6be348
emina.razic <emina.razic@gmail.com>
2026-05-25 17:51:22 +0200

    Home working process

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,14 @@ export default function Home(props) {
         props.setCol2IsVisible(false);
     }, []);
 
-    return (  
-        <div className="wrapper">
-        <BackgroundVideo />      
+    return (
+        <>
+        
+        <div className="wrapper" >
+        <BackgroundVideo />            
+            {/* <div className="col2" style={{flex:"0",opacity:"0.0"}}></div> */}
+        
         </div>          
+        </>
     );
 }
