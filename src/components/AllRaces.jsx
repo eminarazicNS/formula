3080cfd056a4d5bd8fbc0a3cf4be4c24882b062c
emina.razic <emina.razic@gmail.com>
2026-05-27 09:23:54 +0200

    Solving conflict

--- src/components/AllRaces.jsx
+++ src/components/AllRaces.jsx
@@ -11,100 +11,64 @@ export default function AllRaces(props) {
     const [loading, setLoading] = useState(true);
     const [filteredData, setFilteredData] = useState([]);
 
-    export default function AllRaces() {
-        export default function AllRaces(props) {
-            const [races, setRaces] = useState([]);
-            const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
-            useEffect(() => {
-                props.setSearch("");
-                props.setSearchIsVisible(true);
-                props.setSelectIsVisible(true);
-                props.setCol2IsVisible(true);
-            }, []);
+    useEffect(() => {
+        props.setSearch("");
+        props.setSearchIsVisible(true);
+        props.setSelectIsVisible(true);
+        props.setCol2IsVisible(true);
+    }, []);
 
-            useEffect(() => {
-                getRaces();
-            }, [props.year]);
+    useEffect(() => {
+        getRaces();
+    }, [props.year]);
 
 
-            useEffect(() => {
-                getFilteredData();
-            }, [races, props.search]);
+    useEffect(() => {
+        getFilteredData();
+    }, [races, props.search]);
 
 
-            const getRaces = async () => {
-                const url = `https://api.jolpi.ca/ergast/f1/${props.year}/results/1.json`;
-                const response = await axios.get(url);
-                //console.log("races", response.data.MRData.RaceTable.Races);
-                setRaces(response.data.MRData.RaceTable.Races);
-                setLoading(false);
-            }
+    const getRaces = async () => {
+        const url = `https://api.jolpi.ca/ergast/f1/${props.year}/results/1.json`;
+        const response = await axios.get(url);
+        //console.log("races", response.data.MRData.RaceTable.Races);
+        setRaces(response.data.MRData.RaceTable.Races);
+        setLoading(false);
+    }
 
-            const getFilteredData = () => {
-                console.log("getFilteredData");
-                let result = races;
-                //console.log("getFilteredData result ", result);
-                result = result.filter((item) =>
-                    item.raceName.toLowerCase().includes(props.search.toLowerCase()) ||
-                    item.Circuit.circuitName.toLowerCase().includes(props.search.toLowerCase()) ||
-                    item.Results[0].Driver.familyName.toLowerCase().includes(props.search.toLowerCase())
-                );
+    const getFilteredData = () => {
+        console.log("getFilteredData");
+        let result = races;
+        //console.log("getFilteredData result ", result);
+        result = result.filter((item) =>
+            item.raceName.toLowerCase().includes(props.search.toLowerCase()) ||
+            item.Circuit.circuitName.toLowerCase().includes(props.search.toLowerCase()) ||
+            item.Results[0].Driver.familyName.toLowerCase().includes(props.search.toLowerCase())
+        );
 
-                setFilteredData(result);
-            }
+        setFilteredData(result);
+    }
 
-            const handleClick = (id) => {
-                console.log("id", id);
-                navigate(`/raceDetails/${id}`);
-            }
+    const handleClick = (id) => {
+        console.log("id", id);
+        navigate(`/raceDetails/${id}`);
+    }
 
-            if (loading) {
-                return <Loader />;
-            }
+    if (loading) {
+        return <Loader />;
+    }
 
-            const crumbs = [
-                { label: "Races", path: "/races" }
-            ];
+    const crumbs = [
+        { label: "Races", path: "/races" }
+    ];
 
-            return (
-                // <h2>AllRaces</h2>
-                <div className="wrapper">
+    return (
+        // <h2>AllRaces</h2>
+        <div className="wrapper">
 
-        }
-                    return (
-            // <h2>AllRaces</h2>
-                    <div className="wrapper">
 
-<<<<<<< HEAD
-
-            <div className="col2">
-                <h2>RACE CALENDAR</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <td colSpan={3}>Race Calendar - 2013</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {races.map((race, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{race.round}</td>
-                                    <td><Flag country={getFlagByNationality(props.flags, "",
-                                        race.Circuit.Location.country)}
-                                        size={30} /></td>
-                                    <td className="link" onClick={() => handleClick(race.round)}>{race.raceName}</td>
-                                    <td>{race.Circuit.circuitName}</td>
-                                    <td>{race.date}</td>
-                                    <td>{race.Results[0].Driver.familyName}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </div>
-=======
             <div className="col2" className="results">
                 <BasicBreadcrumbs crumbs={crumbs} />
                 <h2>RACE CALENDAR - {props.year}</h2>
@@ -138,8 +102,8 @@ export default function AllRaces(props) {
                         })}
                     </tbody>
                 </table>
->>>>>>> e3de27e5edf95076d0f0a8eb06b1df2cb96b4352
-            </div >
-        );
-        }
+            </div>
+        </div>
+    );
+}
 
