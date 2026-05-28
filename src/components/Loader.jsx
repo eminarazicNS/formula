74f3c9f00f7c1d026fe8d4bddb9cb98353fef192
emina.razic <emina.razic@gmail.com>
2026-05-28 21:22:38 +0200

    Some corrections

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { PuffLoader } from "react-spinners";
 export default function Loader() {
     return (
         <div className="loader-container">
-            <PuffLoader size={100} color="blue"/>
-        </div>    
+            <PuffLoader size={100} color="blue" />
+        </div>
     );
 };
